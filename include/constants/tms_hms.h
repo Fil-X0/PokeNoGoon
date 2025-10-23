56bcd4ea2c726f3d5dce4494683427be64d3ea13
Fil-X0 <felracicot@gmail.com>
2025-10-22 23:17:45 -0400

    TMs Changed

--- include/constants/tms_hms.h
+++ include/constants/tms_hms.h
@@ -2,56 +2,59 @@
 #define GUARD_CONSTANTS_TMS_HMS_H
 
 #define FOREACH_TM(F) \
-    F(FOCUS_PUNCH) \
-    F(DRAGON_CLAW) \
-    F(WATER_PULSE) \
-    F(CALM_MIND) \
-    F(ROAR) \
-    F(TOXIC) \
-    F(HAIL) \
-    F(BULK_UP) \
-    F(BULLET_SEED) \
-    F(HIDDEN_POWER) \
-    F(SUNNY_DAY) \
-    F(TAUNT) \
-    F(ICE_BEAM) \
-    F(BLIZZARD) \
-    F(HYPER_BEAM) \
-    F(LIGHT_SCREEN) \
     F(PROTECT) \
-    F(RAIN_DANCE) \
-    F(GIGA_DRAIN) \
-    F(SAFEGUARD) \
-    F(FRUSTRATION) \
-    F(SOLAR_BEAM) \
-    F(IRON_TAIL) \
-    F(THUNDERBOLT) \
-    F(THUNDER) \
-    F(EARTHQUAKE) \
-    F(RETURN) \
-    F(DIG) \
+    F(BREAKING_SWIPE) \
+    F(SCALD) \
     F(PSYCHIC) \
+    F(SUPER_FANG) \
+    F(TOXIC) \
+    F(FAKE_OUT) \
+    F(SEISMIC_TOSS) \
+    F(MAGICAL_LEAF) \
+    F(ROOST) \
+    F(FLIP_TURN) \
+    F(EXPLOSION) \
+    F(ICE_BEAM) \
+    F(ICICLE_SPEAR) \
+    F(AURA_SPHERE) \
+    F(PSYCHIC_FANGS) \
+    F(LEECH_LIFE) \
+    F(ROCK_BLAST) \
+    F(SOLAR_BLADE) \
+    F(WEATHER_BALL) \
+    F(MUD_SHOT) \
+    F(SOLAR_BEAM) \
+    F(BULLET_PUNCH) \
+    F(THUNDERBOLT) \
+    F(VOLT_SWITCH) \
+    F(EARTHQUAKE) \
+    F(POWER_GEM) \
+    F(DIG) \
+    F(NIGHT_SHADE) \
     F(SHADOW_BALL) \
-    F(BRICK_BREAK) \
-    F(DOUBLE_TEAM) \
-    F(REFLECT) \
+    F(MISTY_EXPLOSION) \
+    F(DRAINING_KISS) \
+    F(DEFOG) \
     F(SHOCK_WAVE) \
-    F(FLAMETHROWER) \
+    F(ELECTROWEB) \
     F(SLUDGE_BOMB) \
-    F(SANDSTORM) \
-    F(FIRE_BLAST) \
+    F(AERIAL_ACE) \
+    F(U_TURN) \
     F(ROCK_TOMB) \
     F(AERIAL_ACE) \
-    F(TORMENT) \
+    F(U_TURN) \
+    F(ROCK_TOMB) \
+    F(DUAL_WINGBEAT) \
+    F(FEINT_ATTACK) \
     F(FACADE) \
-    F(SECRET_POWER) \
-    F(REST) \
-    F(ATTRACT) \
-    F(THIEF) \
-    F(STEEL_WING) \
-    F(SKILL_SWAP) \
-    F(SNATCH) \
-    F(OVERHEAT)
+    F(SMART_STRIKE) \
+    F(SCREECH) \
+    F(METAL_SOUND) \
+    F(DAZZLING_GLEAM) \
+    F(ICY_WIND) \
+    F(TAILWIND) \
+    F(DRAGON_CLAW) \
+    F(FLAMETHROWER)
 
 #define FOREACH_HM(F) \
     F(CUT) \
